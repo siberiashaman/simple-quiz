cdaaf669813b235f5028fc4417ca7e02f62a3963
Dmitry Shestakov <siberiashaman@yandex.ru>
2023-01-11 16:48:52 +0300

    add changes

--- js/quiz.js
+++ js/quiz.js
@@ -3,9 +3,9 @@ const Questions = [{
     id: 0,
     q: "What is capital of India?",
     a: [{ text: "gandhinagar", isCorrect: false },
-        { text: "Surat", isCorrect: false },
-        { text: "Delhi", isCorrect: true },
-        { text: "mumbai", isCorrect: false }
+    { text: "Surat", isCorrect: false },
+    { text: "Delhi", isCorrect: true },
+    { text: "mumbai", isCorrect: false }
     ]
 
 },
@@ -13,19 +13,19 @@ const Questions = [{
     id: 1,
     q: "What is the capital of Thailand?",
     a: [{ text: "Lampang", isCorrect: false, isSelected: false },
-        { text: "phuket", isCorrect: false },
-        { text: "Ayutthaya", isCorrect: false },
-        { text: "Bangkok", isCorrect: true }
+    { text: "phuket", isCorrect: false },
+    { text: "Ayutthaya", isCorrect: false },
+    { text: "Bangkok", isCorrect: true }
     ]
 
 },
 {
     id: 2,
-    q: "What is the capital of Gujarat",
+    q: "What is the capital of Russia",
     a: [{ text: "surat", isCorrect: false },
-        { text: "vadodara", isCorrect: false },
-        { text: "gandhinagar", isCorrect: true },
-        { text: "rajkot", isCorrect: false }
+    { text: "novosibirsk", isCorrect: false },
+    { text: "riga", isCorrect: false },
+    { text: "moscow", isCorrect: true }
     ]
 
 }
@@ -35,94 +35,119 @@ const Questions = [{
 // Set start
 var start = true;
 
+
 // Iterate
 function iterate(id) {
 
-// Getting the result display section
-var result = document.getElementsByClassName("result");
-result[0].innerText = "";
+    // Getting the result display section
+    var result = document.getElementsByClassName("result");
+    result[0].innerText = "";
 
-// Getting the question
-const question = document.getElementById("question");
-
-
-// Setting the question text
-question.innerText = Questions[id].q;
-
-// Getting the options
-const op1 = document.getElementById('op1');
-const op2 = document.getElementById('op2');
-const op3 = document.getElementById('op3');
-const op4 = document.getElementById('op4');
+    // Getting the question
+    const question = document.getElementById("question");
 
 
-// Providing option text 
-op1.innerText = Questions[id].a[0].text;
-op2.innerText = Questions[id].a[1].text;
-op3.innerText = Questions[id].a[2].text;
-op4.innerText = Questions[id].a[3].text;
+    // Setting the question text
+    question.innerText = Questions[id].q;
 
-// Providing the true or false value to the options
-op1.value = Questions[id].a[0].isCorrect;
-op2.value = Questions[id].a[1].isCorrect;
-op3.value = Questions[id].a[2].isCorrect;
-op4.value = Questions[id].a[3].isCorrect;
+    // Getting the options
+    const op1 = document.getElementById('op1');
+    const op2 = document.getElementById('op2');
+    const op3 = document.getElementById('op3');
+    const op4 = document.getElementById('op4');
 
-var selected = "";
 
-// Show selection for op1
-op1.addEventListener("click", () => {
-    op1.style.backgroundColor = "lightgoldenrodyellow";
-    op2.style.backgroundColor = "lightskyblue";
-    op3.style.backgroundColor = "lightskyblue";
-    op4.style.backgroundColor = "lightskyblue";
-    selected = op1.value;
-})
+    // Providing option text 
+    op1.innerText = Questions[id].a[0].text;
+    op2.innerText = Questions[id].a[1].text;
+    op3.innerText = Questions[id].a[2].text;
+    op4.innerText = Questions[id].a[3].text;
 
-// Show selection for op2
-op2.addEventListener("click", () => {
-    op1.style.backgroundColor = "lightskyblue";
-    op2.style.backgroundColor = "lightgoldenrodyellow";
-    op3.style.backgroundColor = "lightskyblue";
-    op4.style.backgroundColor = "lightskyblue";
-    selected = op2.value;
-})
+    // Providing the true or false value to the options
+    op1.value = Questions[id].a[0].isCorrect;
+    op2.value = Questions[id].a[1].isCorrect;
+    op3.value = Questions[id].a[2].isCorrect;
+    op4.value = Questions[id].a[3].isCorrect;
 
-// Show selection for op3
-op3.addEventListener("click", () => {
-    op1.style.backgroundColor = "lightskyblue";
-    op2.style.backgroundColor = "lightskyblue";
-    op3.style.backgroundColor = "lightgoldenrodyellow";
-    op4.style.backgroundColor = "lightskyblue";
-    selected = op3.value;
-})
+    var selected = "";
 
-// Show selection for op4
-op4.addEventListener("click", () => {
-    op1.style.backgroundColor = "lightskyblue";
-    op2.style.backgroundColor = "lightskyblue";
-    op3.style.backgroundColor = "lightskyblue";
-    op4.style.backgroundColor = "lightgoldenrodyellow";
-    selected = op4.value;
-})
+    // Show selection for op1
+    op1.addEventListener("click", () => {
+        if (op1.classList.contains("btn-primary")) {
+            op1.classList.add("btn-warning");
+            op1.classList.remove("btn-primary");
+            op2.classList.remove("btn-warning");
+            op2.classList.add("btn-primary");
+            op3.classList.remove("btn-warning");
+            op3.classList.add("btn-primary");
+            op4.classList.remove("btn-warning");
+            op4.classList.add("btn-primary");
+        }
+        selected = op1.value;
+    })
 
-// Grabbing the evaluate button
-const evaluate = document.getElementsByClassName("evaluate");
+    // Show selection for op2
+    op2.addEventListener("click", () => {
+        if (op2.classList.contains("btn-primary")) {
+            op2.classList.add("btn-warning");
+            op2.classList.remove("btn-primary");
+            op1.classList.remove("btn-warning");
+            op1.classList.add("btn-primary");
+            op3.classList.remove("btn-warning");
+            op3.classList.add("btn-primary");
+            op4.classList.remove("btn-warning");
+            op4.classList.add("btn-primary");
+        }
+        selected = op2.value;
+    })
 
-// Evaluate method
-evaluate[0].addEventListener("click", () => {
-    if (selected == "true") {
-        result[0].innerHTML = "True";
-        result[0].style.color = "green";
-    } else {
-        result[0].innerHTML = "False";
-        result[0].style.color = "red";
-    }
-})
+    // Show selection for op3
+    op3.addEventListener("click", () => {
+        if (op3.classList.contains("btn-primary")) {
+            op3.classList.add("btn-warning");
+            op3.classList.remove("btn-primary");
+            op1.classList.remove("btn-warning");
+            op1.classList.add("btn-primary");
+            op2.classList.remove("btn-warning");
+            op2.classList.add("btn-primary");
+            op4.classList.remove("btn-warning");
+            op4.classList.add("btn-primary");
+        }
+        selected = op3.value;
+    })
+
+    // Show selection for op4
+    op4.addEventListener("click", () => {
+        if (op4.classList.contains("btn-primary")) {
+            op4.classList.add("btn-warning");
+            op4.classList.remove("btn-primary");
+            op1.classList.remove("btn-warning");
+            op1.classList.add("btn-primary");
+            op2.classList.remove("btn-warning");
+            op2.classList.add("btn-primary");
+            op3.classList.remove("btn-warning");
+            op3.classList.add("btn-primary");
+        }
+        selected = op4.value;
+    })
+
+    // Grabbing the evaluate button
+    const evaluate = document.getElementsByClassName("evaluate");
+
+    // Evaluate method
+    evaluate[0].addEventListener("click", () => {
+        if (selected == "true") {
+            result[0].innerHTML = "True";
+            result[0].style.color = "green";
+        } else {
+            result[0].innerHTML = "False";
+            result[0].style.color = "red";
+        }
+    })
 }
 
 if (start) {
-iterate("0");
+    iterate("0");
 }
 
 // Next button and method
@@ -130,12 +155,11 @@ const next = document.getElementsByClassName('next')[0];
 var id = 0;
 
 next.addEventListener("click", () => {
-start = false;
-if (id < 2) {
-    id++;
-    iterate(id);
-    console.log(id);
-}
+    start = false;
+    if (id < 2) {
+        id++;
+        iterate(id);
+        console.log(id);
+    }
 
 })
-Output:
